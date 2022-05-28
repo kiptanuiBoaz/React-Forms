2c506393e62b45daee929362381852a126ce5101
boaz <boaserem022@gmail.com>
2022-05-28 15:25:23 +0300

    second commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+
+  const [name,setName] = useState("");
+  const [heading,setHeading] = useState("");
+
+  function handleChange(event){
+    
+    setName(event.target.value);
+  
+  };
+
+  function handleClick(event){
+    setHeading(name);
+    event.preventDefault();
+  };
+
+
   return (
-    <div className="container">
-      <h1>Hello </h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
-    </div>
+    <form onSubmit = {handleClick}>
+      <div className="container">
+        <h1>Hello {heading}</h1>
+        <input onChange ={handleChange} value ={name} type="text" placeholder="What's your name?" />
+        <button type = "submit">Submit</button>
+      </div>
+    </form>
+    
+      
   );
 }
 
